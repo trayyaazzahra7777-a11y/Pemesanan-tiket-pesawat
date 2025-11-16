d18bfe92c669664c4e9832bbfd8518b96bd588e9
Hylmii08 <afrahdhanadyaksa@gmail.com>
2025-11-16 20:49:23 +0800

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -33,3 +33,41 @@ void tampilRiwayat() {
         cout << "Kode Promo: " << (riwayat[i].kodePromo.empty() ? "-" : riwayat[i].kodePromo) << endl;
         cout << "Harga: Rp" << riwayat[i].harga << endl;
     }
+void pesanTiket() {
+    Pemesanan p;
+    int pilihanTujuan, pilihanKelas, pilihanTransit;
+    double hargaDasar = 0;
+
+    cout << "\n=== PEMESANAN TIKET PESAWAT ===\n";
+    cout << "Masukkan nama anda: ";
+    getline(cin >> ws, p.nama);
+
+    cout << "\nPilih tujuan penerbangan:\n";
+    cout << "1. Makassar - Jakarta (RP. 1.200.000)\n";
+    cout << "2. Palu - Makassar (RP. 900.000)\n";
+    cout << "3. Jakarta - Malaysia (RP. 2.500.000)\n";
+    cout << "Pilihan: ";
+    cin >> pilihanTujuan;
+
+    switch (pilihanTujuan) {
+        case 1: p.tujuan = "Makassar - Jakarta"; hargaDasar = 1200000; break;
+        case 2: p.tujuan = "Palu - Makassar"; hargaDasar = 900000; break;
+        case 3: p.tujuan = "Jakarta - malaysia"; hargaDasar = 2500000; break;
+        default: cout << "Pilihan tidak valid!\n"; return;
+    }
+
+    cout << "\nPilih kelas penerbangan:\n";
+    cout << "1. Economy Class\n";
+    cout << "2. Business Class\n";
+    cout << "Pilihan: ";
+    cin >> pilihanKelas;
+
+    if (pilihanKelas == 1) {
+        p.kelas = "Economy Class";
+        hargaDasar *= 1.0;
+    } else if (pilihanKelas == 2) {
+        p.kelas = "Business Class";
+        hargaDasar *= 1.75;
+    } else {
+        cout << "Pilihan tidak valid!\n"; return;
+    }
